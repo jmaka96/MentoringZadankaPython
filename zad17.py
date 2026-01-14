5322abf3eebca06ed3e0ff963dd54805b99e6763
Jakub <jakubmaka3@gmail.com>
2026-01-14 15:07:31 +0100

    Updated excercise to include car count

--- zad17.py
+++ zad17.py
@@ -1,10 +1,18 @@
+car_count = 0
+
 class Car:
-    def __init__(self,marka,model,rok):
+    def __init__(self, marka, model, rok):
+        global car_count
         self.marka = marka
         self.model = model
         self.rok = rok
+        car_count += 1
     def describe(self):
         print("Dane samochodu. Marka: "+ self.marka + ". model: " + self.model + ". rok produkcji: " + str(self.rok))
+
+    def print_count():
+        global car_count
+        print("liczba utworzonych samochodów: " + str(car_count))
 
 car1 = Car("Toyota", "Yaris", 2020)
 car1.describe()
@@ -14,3 +22,5 @@ car2.describe()
 
 car3 = Car("Ford", "Focus", 1996)
 car3.describe()
+
+Car.print_count()
